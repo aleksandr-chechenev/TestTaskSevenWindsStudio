d577397734f007d363dd232f12643bf8dd3a3f0c
aleksandr-chechenev <aleksandrchechenev12@gmail.com>
2022-12-16 21:26:47 +0300

    Add Swagger

--- src/main/java/com/example/testtasksevenwindsstudio/controller/PersonalDataController.java
+++ src/main/java/com/example/testtasksevenwindsstudio/controller/PersonalDataController.java
@@ -3,6 +3,8 @@ package com.example.testtasksevenwindsstudio.controller;
 import com.example.testtasksevenwindsstudio.dto.AddPersonalDataRequestDto;
 import com.example.testtasksevenwindsstudio.entity.PersonalData;
 import com.example.testtasksevenwindsstudio.service.PersonalDataService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,23 +12,27 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/info")
+@Api
 public class PersonalDataController {
     @Autowired
     PersonalDataService personalDataService;
 
     @GetMapping
+    @ApiOperation("Получение всех записей")
     public List<PersonalData> getAllPersonalData() {
         List<PersonalData> personalDataList = personalDataService.getAllPersonalData();
         return personalDataList;
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Получение записи по id")
     public PersonalData getPersonalDataById(@PathVariable int id) {
         PersonalData personalData = personalDataService.getPersonalDataById(id);
         return personalData;
     }
 
     @PostMapping
+    @ApiOperation("Сохранение записи в базу данных")
     public void addPersonalData(@RequestBody AddPersonalDataRequestDto personalDataDto) {
         personalDataService.savePersonalData(personalDataDto);
     }
